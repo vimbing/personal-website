6852ca3bbd2432785598dbe910a7865a0b0e0d54
vimbing <jakubbubak9@gmail.com>
2025-03-17 03:27:14 +0100

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,60 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import githubIcon from "../../public/github-logo.svg"
+import { Geist_Mono } from "next/font/google";
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+function TerminalBlinker() {
+  const [hidden, setHidden] = useState<boolean>(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => setHidden(prev => !prev), 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return <div className={`w-[2.5rem] mx-3 h-[4rem] ${hidden ? "bg-transparent" : "bg-purple-200"}`}></div>
+}
+
+function TerminalText({ text }: { text: string, }) {
+  const [renderedText, setRenderedText] = useState("");
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (renderedText.length == text.length) return;
+      setRenderedText(prev => text.slice(0, prev.length + 1));
+    }, 50);
+    return () => clearTimeout(timeout);
+  }, [renderedText]);
+
+  return <div className="flex">
+    <p className="text-5xl tracking-widest">{renderedText}</p>
+    <TerminalBlinker />
+  </div>
+}
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className={`p-5 md:p-[5rem] min-w-screen min-h-screen flex flex-col items-center justify-center gap-y-5 ${geistMono.className} text-purple-300`}>
+      <div className="flex flex-col">
+        <TerminalText text="bmbng _" />
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <div>
+        <p className="text-md border p-3" >
+          I am a highly motivated person interested in software development, especially in web scraping and automating. I started my programming journey when I was 15 by contributing to the sneaker botting community and taking part in an arms race of the sneaker bot market. I also love to self-host software, develop my own solutions for all kinds of problems, and learn how to build various projects from scratch.
+        </p>
+      </div>
+
+      <div className="w-full flex flex-col">
+        <a target="_blank" className="underline" href="https://github.com/vimbing">{">"} github</a>
+        <p>{">"} jakubbubak9@gmail.com</p>
+      </div>
     </div>
   );
 }
