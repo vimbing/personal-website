ccf844482b025cc072fd70ab789c08d085b9cc2f
vimbing <jakubbubak9@gmail.com>
2025-03-18 03:31:50 +0100

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,31 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+interface Config {
+  appearance: {
+    blinkerDelay?: number,
+    typingDelay?: number,
+  }
+  description: string
+  githubUrl: string
+  email: string
+}
+
+const config = {
+  description: "I am a highly motivated person interested in software development, especially in web scraping and automating. I started my programming journey when I was 15 by contributing to the sneaker botting community and taking part in an arms race of the sneaker bot market. I also love to self-host software, develop my own solutions for all kinds of problems, and learn how to build various projects from scratch.",
+  githubUrl: "https://github.com/vimbing",
+  email: "jakubbubak9@gmail.com",
+  appearance: {
+    blinkerDelay: 1000,
+    typingDelay: 75
+  }
+} as Config;
+
 function TerminalBlinker() {
   const [hidden, setHidden] = useState<boolean>(false);
 
   useEffect(() => {
-    const interval = setInterval(() => setHidden(prev => !prev), 1000);
+    const interval = setInterval(() => setHidden(prev => !prev), config.appearance.blinkerDelay || 1000);
     return () => clearInterval(interval);
   }, []);
 
@@ -26,13 +46,22 @@ function TerminalText({ text }: { text: string, }) {
     const timeout = setTimeout(() => {
       if (renderedText.length == text.length) return;
       setRenderedText(prev => text.slice(0, prev.length + 1));
-    }, 50);
+    }, config.appearance.typingDelay || 75);
     return () => clearTimeout(timeout);
   }, [renderedText]);
 
   return <div className="flex">
     <p className="text-5xl tracking-widest">{renderedText}</p>
     <TerminalBlinker />
+  </div>
+}
+
+function Section({ heading, children }: Readonly<{ heading: string, children: React.ReactNode }>) {
+  return <div className="w-full flex flex-col">
+    <p>{">"} {heading}</p>
+    <div className="ml-8">
+      {children}
+    </div>
   </div>
 }
 
@@ -49,10 +78,12 @@ export default function Home() {
         </p>
       </div>
 
-      <div className="w-full flex flex-col">
-        <a target="_blank" className="underline" href="https://github.com/vimbing">{">"} github</a>
-        <p>{">"} jakubbubak9@gmail.com</p>
-      </div>
+      <Section heading="contact">
+        <div>
+          <a target="_blank" className="underline" href={config.githubUrl}>{">"} github</a>
+          <p>{">"} {config.email}</p>
+        </div>
+      </Section>
     </div>
   );
 }
